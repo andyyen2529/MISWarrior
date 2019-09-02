5e356f41f7c293936521aa6d3b627a54c03da85b
SwordenUC <kevin123uuu000@yahoo.com.tw>
2019-09-02 22:19:34 +0800

    將0050股市資料加入資料庫(db.sqlite3)中(試作版)
    更新者：陳宇鑫
    新增model：user、stock，其中user只有一筆資料(name = '陳宇鑫')；stock只有0050的股市資料
    備註：
    1. 因為技術限制，目前資料格式必須如同stock.xlsx這樣，必須自訂id，而且不像原本是存成csv，因為存成csv會讓股票代碼的欄位跑掉(0050 --> 50)
    2. 欄位中英文對照(如果覺得某些欄位翻譯不到位可以通知宇鑫喔)
    	code = 股票代碼
    	date = 日期
    	volumn = 成交股數
    	turnover = 成交金額
    	opening_price = 開盤價
    	high = 最高價
    	low = 最低價
    	closeing_price = 收盤價
    	spread = 漲跌價差
    	transaction_number = 成交筆數

--- TradingAgent/TradingGame/models.py
+++ TradingAgent/TradingGame/models.py
@@ -12,3 +12,27 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 30)
+    last_modify_date = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "user"
+
+class Stock(models.Model):
+	code = models.CharField(max_length = 30) # 股票代碼
+	date = models.DateField() # 日期
+	volumn = models.IntegerField() # 成交股數
+	turnover = models.IntegerField() # 成交金額
+	opening_price = models.FloatField() # 開盤價
+	high = models.FloatField() # 最高價
+	low = models.FloatField() # 最低價
+	closeing_price = models.FloatField() # 收盤價
+	spread = models.FloatField() # 漲跌價差
+	transaction_number = models.IntegerField() # 成交筆數
+
+	class Meta:
+		db_table = "stock"
