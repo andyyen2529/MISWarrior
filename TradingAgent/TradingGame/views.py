2a2e52450e8f1b59428fdc0a5348fb0d9b5423db
andyyen2529 <31908052+andyyen2529@users.noreply.github.com>
2019-09-03 13:28:16 +0800

    test

--- TradingAgent/TradingGame/views.py
+++ TradingAgent/TradingGame/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're 是 87")
+    return HttpResponse("Hello, world. You're 是 887")
 	
 def index2(request):
     return render(request, 'index.html', {'question': 'aaa'})
