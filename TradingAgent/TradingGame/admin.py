2c95841fef5a5dd479b922c515887998263d4de0
SwordenUC <kevin123uuu000@yahoo.com.tw>
2019-09-08 17:50:13 +0800

    修正及新增資料庫的部分
    新增的model：
    (1) History (交易歷史)
    (2) Setup (交易設定)

--- TradingAgent/TradingGame/admin.py
+++ TradingAgent/TradingGame/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from TradingGame.models import User
-from TradingGame.models import Stock
+from TradingGame.models import User, Stock, History, Setup
 
 # ModelAdmin class
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ImportExportModelAdmin):
 	list_display = ('id', 'name', 'last_modify_date', 'created')
 
 class StockAdmin(ImportExportModelAdmin):
 	list_display = ('id', 'code', 'date', 'volumn', 'turnover', 'opening_price', 
 		'high', 'low', 'closeing_price', 'spread', 'transaction_number')
 
+class HistoryAdmin(ImportExportModelAdmin):
+	list_display = ('id', 'user', 'stock', 'position_before_action', 
+		'rate_of_return_before_action', 'action')
+
+class SetupAdmin(ImportExportModelAdmin):
+	list_display = ('id', 'user', 'initial_transaction_date', 'playing_duration',
+		'principal', 'transaction_cost_rate')
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Stock, StockAdmin)
+admin.site.register(History, HistoryAdmin)
+admin.site.register(Setup, SetupAdmin)
--- TradingAgent/TradingGame/models.py
+++ TradingAgent/TradingGame/models.py
@@ -1,27 +1,18 @@
 from django.db import models
 
+### 用戶 ###
+class User(models.Model): 
+	name = models.CharField(max_length = 30)
+	last_modify_date = models.DateTimeField(auto_now=True)
+	created = models.DateTimeField(auto_now_add=True)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
+	class Meta:
+		db_table = 'user'
 
+	def __str__(self):
+		return self.name
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-# Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length = 30)
-    last_modify_date = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "user"
-
+### 股票資料 ###
 class Stock(models.Model):
 	code = models.CharField(max_length = 30) # 股票代碼
 	date = models.DateField() # 日期
@@ -35,4 +26,30 @@ class Stock(models.Model):
 	transaction_number = models.IntegerField() # 成交筆數
 
 	class Meta:
-		db_table = "stock"
+		db_table = 'stock'
+
+	def __str__(self):
+		return '股票代碼：' + str(self.code) + '；日期：' + str(self.date)
+
+### 交易歷史 ###
+class History(models.Model):
+	user = models.ForeignKey(User, on_delete = models.CASCADE) # 用戶編號(外來鍵)
+	stock = models.ForeignKey(Stock, related_name = 'stocks', on_delete = models.CASCADE) # 股票資料編號(外來鍵)
+	position_before_action = models.CharField(max_length = 1, choices = (('0', '現金'), ('1', '股票'))) # 行動之前的資產持有狀態
+	rate_of_return_before_action = models.FloatField() # 行動之前的報酬率	
+	action = models.CharField(max_length = 1, choices = (('0', '等待or持有'), ('1', '買or賣'))) # 行動
+
+	class Meta:
+		db_table = 'history'
+
+### 交易設定 ###
+class Setup(models.Model):
+	user = models.ForeignKey(User, on_delete = models.CASCADE) # 用戶編號(外來鍵)
+	stock_code = models.CharField(max_length = 4, choices = (('0050', '元大台灣50'), ('2430', '燦坤'))) # 股票代碼
+	initial_transaction_date = models.DateField() # 起始交易日
+	playing_duration = models.IntegerField() # 遊玩天數(只計股市交易日)
+	principal = models.IntegerField() # 本金
+	transaction_cost_rate = models.FloatField() # 交易成本比率
+
+	class Meta:
+		db_table = 'setup'
